f9fc0fc1f4b83fb236380da517e499929c3696b9
Devon P Humphreys <dev.humphreys@gmail.com>
2022-08-04 11:38:25 -0500

    Create git_practice.py
    Adding a new, non-README file.

---
+++ git_practice.py
@@ -0,0 +1,11 @@
+# Filename: git_practice.py
+# Purpose: a simple Python3 program to print odd numbers between two provided values
+
+min = 1
+max = 99
+
+for i in range(min, max):
+  if i % 2 == 0: # use modulus operator to determine parity of i
+    pass
+  else: 
+    print(i)
